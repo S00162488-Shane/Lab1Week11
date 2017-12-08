488f5b9b2ccfc194d2fc774b5fa76cde4ad18bc2
S00162488 <S00162488@mail.itsligo.ie>
2017-12-08 11:24:21 +0000

    Class Changes

---
+++ Lab1Week11/Migrations/ClubMigrations/201712081057036_Date Nullified.cs
@@ -0,0 +1,16 @@
+namespace Lab1Week11.Migrations.ClubMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DateNullified : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
